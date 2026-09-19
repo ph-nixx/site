5ac3e68aed8cac923606668b547416cbb4861189
ph-nixx <pmiller0706@gmail.com>
2026-09-19 15:56:08 -0500

    chore: directory project management

---
+++ pkg/flavor/doc.go
@@ -0,0 +1,4 @@
+// The most efficient GFM to HTML parser built with Go. (you have to prove this with benchmarks against the standard)
+// We want the interface to be as declarative and unopinionated as possible.
+
+package flavor
